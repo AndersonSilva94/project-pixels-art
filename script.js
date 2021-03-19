806c93b6d70f46be2722a658b5d4c12c34aae61a
andy silva <andssilva.23@gmail.com>
2021-03-18 20:21:23 -0400

    ajeitando os estilos e adicionando background-image

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const pixelBoard = document.querySelector('#pixel-board');
-function createColorsPallete(amount) {
+
+function createBasicColors(amount) {
   const divColors = document.querySelector('#color-palette');
   for (let index = 1; index <= amount; index += 1) {
     const colors = document.createElement('div');
@@ -7,12 +8,29 @@ function createColorsPallete(amount) {
     divColors.appendChild(colors);
   }
 }
-createColorsPallete(10);
+createBasicColors(8);
+
+function createRandonColors(amount) {
+  const divColors = document.querySelector('#color-palette2');
+  for (let index = 1; index <= amount; index += 1) {
+    const colors = document.createElement('div');
+    colors.className = 'color';
+    divColors.appendChild(colors);
+  }
+}
+createRandonColors(10);
 
 function assignColors() {
   const colors = document.querySelectorAll('.color');
-  colors[0].style.backgroundColor = 'black';
-  for (let index = 1; index < colors.length; index += 1) {
+  colors[0].style.backgroundColor = 'rgb(255, 0, 0)';
+  colors[1].style.backgroundColor = 'rgb(255, 127, 0)';
+  colors[2].style.backgroundColor = 'rgb(255, 255, 0)';
+  colors[3].style.backgroundColor = 'rgb(0, 255, 0)';
+  colors[4].style.backgroundColor = 'rgb(0, 0, 255)';
+  colors[5].style.backgroundColor = 'rgb(75, 0, 130)';
+  colors[6].style.backgroundColor = 'rgb(143, 0, 255)';
+  colors[7].style.backgroundColor = 'rgb(0, 0, 0)';
+  for (let index = 8; index < colors.length; index += 1) {
     const colorRed = Math.floor(Math.random() * 255);
     const colorGreen = Math.floor(Math.random() * 255);
     const colorBlue = Math.floor(Math.random() * 255);
@@ -40,12 +58,17 @@ document.querySelectorAll('.color')[0].className += ' selected';
 // Ajuda mútua, em especial ao Murilo Gonçalves, turma 10 - tribo A na explicação sobre event
 function changeSelected() {
   const classSelected = document.querySelector('#color-palette');
-  classSelected.addEventListener('click', (event) => {
+  const classSelected2 = document.querySelector('#color-palette2');
+  
+  classSelected.addEventListener('click', changeColors);
+  classSelected2.addEventListener('click', changeColors)
+
+  function changeColors(event) {
     const classColor = event.target;
     const elementSelected = document.getElementsByClassName('selected');
     elementSelected[0].classList.remove('selected');
     classColor.classList.add('selected');
-  });
+  }
 }
 changeSelected();
 
